382a3e018883918037e0f3156f0e368706395f3c
ZXCShval <dmitriy.zargot@gmail.com>
2021-12-20 21:09:13 +0400

    Я сделал первое задание сука

---
+++ src/main/java/syntax/lesson01/PrimitivesSSs.java
@@ -0,0 +1,11 @@
+package syntax.lesson01;
+
+public class PrimitivesSSs {
+    public static void main (String[] args) {
+        {char huy = 'a';
+            System.out.println(huy);
+        }
+        int twoPow = 2;
+        System.out.println(twoPow*twoPow*twoPow*twoPow*twoPow);
+    }
+}
