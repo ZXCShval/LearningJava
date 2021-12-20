6d11c975c840a4029cf20d105b8fea00a82331d6
ZXCShval <dmitriy.zargot@gmail.com>
2021-12-20 21:35:13 +0400

    Добавил метод, добавил и инициировал char, добавил int, умножил в 5ю степень двумя способами.

--- src/main/java/syntax/lesson01/PrimitivesSSs.java
+++ src/main/java/syntax/lesson01/PrimitivesSSs.java
@@ -1,11 +1,15 @@
 package syntax.lesson01;
 
 public class PrimitivesSSs {
+
     public static void main (String[] args) {
-        {char huy = 'a';
-            System.out.println(huy);
-        }
+        char huy = 'a';
+        System.out.println(huy);
+        {
         int twoPow = 2;
         System.out.println(twoPow*twoPow*twoPow*twoPow*twoPow);
+        System.out.println(twoPow<<twoPow<<twoPow);
     }
 }
+}
+
