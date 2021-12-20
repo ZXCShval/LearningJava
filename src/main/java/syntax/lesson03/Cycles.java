29f40407e7d620262ee00bee761487bd8f3d1544
ZXCShval <dmitriy.zargot@gmail.com>
2021-12-20 22:20:51 +0400

    Научил Канеки вычитанию через for

---
+++ src/main/java/syntax/lesson03/Cycles.java
@@ -0,0 +1,9 @@
+package syntax.lesson03;
+
+public class Cycles {
+    public static void main (String[] args) {
+        for (int Ken = 1000; Ken > 0; Ken-=7){
+            System.out.println(Ken);
+        }
+    }
+}
