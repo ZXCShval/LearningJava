fcad21b391012c94f9b6407ad17f57b6cdc610b8
ZXCShval <dmitriy.zargot@gmail.com>
2021-12-20 22:11:38 +0400

    Завел сВИЧ кейсы под дни недели.

--- src/main/java/syntax/lesson02/Conditionals.java
+++ src/main/java/syntax/lesson02/Conditionals.java
@@ -1,5 +1,7 @@
 package syntax.lesson02;
 
+import java.time.LocalDate;
+
 public class Conditionals {
     public static void main (String[] args) {
         int MyFavNumber = 69;
@@ -12,5 +14,16 @@ public class Conditionals {
         if (MyFavNumber > 42){
             System.out.println(MyFavNumber-42);
         }
+        int dayOfWeek = LocalDate.now().getDayOfWeek().getValue();
+        switch(dayOfWeek) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6, 7 -> System.out.println("Weekend");
+        }
+
+        }
     }
-}
+
