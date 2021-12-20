0a449c58ee039084b39a1123ca4bc135459f2db6
ZXCShval <dmitriy.zargot@gmail.com>
2021-12-20 23:08:19 +0400

    Канеки вычитает через while + сороконожка через if.

--- src/main/java/syntax/lesson03/Cycles.java
+++ src/main/java/syntax/lesson03/Cycles.java
@@ -1,9 +1,16 @@
 package syntax.lesson03;
 
 public class Cycles {
-    public static void main (String[] args) {
-        for (int Ken = 1000; Ken > 0; Ken-=7){
-            System.out.println(Ken);
+    public static void main(String[] args) {
+        {
+            int Ken = 1000;
+            while (Ken > 0) {
+                System.out.println(Ken);
+                Ken -= 7;
+                if (Ken < 400 & Ken > 393){
+                    System.out.println("Канеки-кун, тебя ждет сороконожка в ухо, сука");
+                }
+            }
         }
-    }
+}
 }
