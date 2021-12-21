3564bd06463b9a2c68804309ea0201cab2fc617f
ZXCShval <dmitriy.zargot@gmail.com>
2021-12-21 21:12:25 +0400

    Через метод replaceAll заменил сначала "о",затем ",".

--- src/main/java/syntax/lesson04/Strings.java
+++ src/main/java/syntax/lesson04/Strings.java
@@ -1,11 +1,14 @@
 package syntax.lesson04;
 
 public class Strings {
-   public static void main(String[] args) {
+
+    public static void main(String[] args) {
        String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n" +
                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n" +
                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n" +
                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
+        String s = lorem.replaceAll("o", "ˤ̙̘̗̖̔̒̓̑̐̏̎̍̚̕");
+       final String m = s.replaceAll(",","ˤ̙̘̗̖̔̒̓̑̐̏̎̍̚̕");
+       System.out.println(m);
    }
 }
