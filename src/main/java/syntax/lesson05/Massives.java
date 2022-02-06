5a65c987b3a9b8837d4f7b1f0241a79440be50f4
ZXCShval <dmitriy.zargot@gmail.com>
2022-02-06 19:40:39 +0400

    Разбито на методы через boolean

--- src/main/java/syntax/lesson05/Massives.java
+++ src/main/java/syntax/lesson05/Massives.java
@@ -3,10 +3,11 @@ import java.util.Arrays;
 
 public class Massives {
     public static void main (String[] args) {
-        Bubble();
+        bubble(true);
+        bubble2();
         Lorem();
     }
-        static void Bubble () {
+        static void bubble (boolean f) {
         float[] poryadok = {5f, 6f, 2f, 1f, 9f, 15f, 3f, 4f, 7f, 8f, 11f, 20f, 17f, 19f, 16f};
         int dataLength = poryadok.length;
         float swap;
@@ -16,19 +17,32 @@ public class Massives {
             sorted = true;
 
             for (int a = 1; a < (dataLength - i); a++) {
+                boolean condom;
+                if (f) {
+                    condom = poryadok[a - 1] > poryadok[a];
+                }
+                else {
+                    condom = poryadok[a - 1] < poryadok[a];
+                }
 
-                if (poryadok[a - 1] < poryadok[a]) {
+                if (condom) {
                     swap = poryadok[a - 1];
                     poryadok[a - 1] = poryadok[a];
                     poryadok[a] = swap;
-                    System.out.println("Шаг внешнего цикла = " + i + ". Шаг внутреннего цикла = "+ a +". Меняю местами " + poryadok[a-1] + " и " + poryadok[a]);
+                    System.out.println("Шаг внешнего цикла = " + i + ". Шаг внутреннего цикла = " + a + ". Меняю местами " + poryadok[a - 1] + " и " + poryadok[a]);
                     sorted = false;
                 }
             }
             if (sorted) break;
         }
         System.out.println(Arrays.toString(poryadok));
+
     }
+        static void bubble2() {
+            bubble(false);
+
+
+        }
 
         static void Lorem()
         {
